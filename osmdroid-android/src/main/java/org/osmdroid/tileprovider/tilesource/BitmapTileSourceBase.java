23cd9dc07e22abb7656d739580bba78a8036cc36
Alexey Illarionov <littlesavage@rambler.ru>
2014-03-13 00:59:20 +0400

    Throw LowMemoryException on getDrawable(final String aFilePath)

--- osmdroid-android/src/main/java/org/osmdroid/tileprovider/tilesource/BitmapTileSourceBase.java
+++ osmdroid-android/src/main/java/org/osmdroid/tileprovider/tilesource/BitmapTileSourceBase.java
@@ -97,7 +97,7 @@ public abstract class BitmapTileSourceBase implements ITileSource,
 	}
 
 	@Override
-	public Drawable getDrawable(final String aFilePath) {
+	public Drawable getDrawable(final String aFilePath) throws LowMemoryException {
 		Bitmap bitmap;
 		BitmapFactory.Options bitmapOptions;
 		try {
@@ -125,6 +125,7 @@ public abstract class BitmapTileSourceBase implements ITileSource,
 		} catch (final OutOfMemoryError e) {
 			logger.error("OutOfMemoryError loading bitmap: " + aFilePath);
 			System.gc();
+			throw new LowMemoryException(e);
 		}
 		return null;
 	}
